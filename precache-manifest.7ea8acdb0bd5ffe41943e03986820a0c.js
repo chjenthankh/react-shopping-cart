707b42ec461ba9c13ef13d79fe74f2b6e8e407ad
chjenthankh <chjenthankh@gmail.com>
2020-10-03 23:02:27 +0700

    Updates

---
+++ precache-manifest.7ea8acdb0bd5ffe41943e03986820a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d30ba3118f9d05db356f190d3753f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bb2effaff0e9c89edb9",
+    "url": "/static/css/main.edd48e2f.chunk.css"
+  },
+  {
+    "revision": "d0522aa6fe66ac472e67",
+    "url": "/static/js/2.3a356f50.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.3a356f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb2effaff0e9c89edb9",
+    "url": "/static/js/main.de2e046a.chunk.js"
+  },
+  {
+    "revision": "75b67e50613f7dd4cc1e",
+    "url": "/static/js/runtime-main.5a1d5d0f.js"
+  }
+]);
